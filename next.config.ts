3f8dfee4b7355116d0873e7172bf23bedd8a65f3
unknown <tuongkhai.nguyen@gmail.com>
2025-10-24 23:19:07 -0400

    change ci/cd

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'nova-wushu-academy';
+const nextConfig = {
   output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/nova-wushu-academy' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/nova-wushu-academy' : '',
+  images: { unoptimized: true },
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
 };
 
-export default nextConfig;
+module.exports = nextConfig;

dac2539513b91a3be90cef19973539312986e48d
kaifive <kaifive@users.noreply.github.com>
2025-10-25 03:06:03 +0000

    deploy: 9f44fd7b4b1273d942dbbbd602017e8d92372f0c

---
+++ _next/static/gf1qg7COlqhR5mBB8i5Dp/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/fe3aa4b8401d9fe2.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

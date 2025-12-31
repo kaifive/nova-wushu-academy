234e0909e0f64c3ff06fac38013ada76fecec60e
kaifive <kaifive@users.noreply.github.com>
2025-12-31 21:04:22 +0000

    deploy: 906e53f63450d8391021a25e28cbf503f6c7c624

---
+++ _next/static/HEj9eQNlYCcyqO-clgUKE/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/7e17b1aae0dc5983.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/nova-wushu-academy//_next/:path+",
+        "destination": "/nova-wushu-academy/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

a2d9be57bc35510ab5f88047636adb4c07f0fc4c
kaifive <kaifive@users.noreply.github.com>
2026-01-02 03:21:44 +0000

    deploy: df4eaf33e8cde407f4e4104713eacf91feb51916

--- _next/static/_GJLb4mqgJgGpKTDCtY5i/_buildManifest.js
+++ _next/static/_GJLb4mqgJgGpKTDCtY5i/_buildManifest.js
@@ -1,15 +1,10 @@
 self.__BUILD_MANIFEST = {
   "/_error": [
-    "./static/chunks/7e17b1aae0dc5983.js"
+    "./static/chunks/7b1d812700085db8.js"
   ],
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/nova-wushu-academy//_next/:path+",
-        "destination": "/nova-wushu-academy/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
---
+++ _next/static/chunks/e21e2850d8a52915.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/8848aad00a5f1d14.js",
+  "static/chunks/turbopack-f447b9c8a15493b2.js"
+])
